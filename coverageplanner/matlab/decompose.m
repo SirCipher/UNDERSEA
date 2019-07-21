218c1938fb51994a993b219333cf7328bf2ef401
SirCipher <tk734@york.ac.uk>
2019-07-21 15:14:34 +0100

    Updates decomposer MATLAB script

--- coverageplanner/matlab/decompose.m
+++ coverageplanner/matlab/decompose.m
@@ -1,11 +1,11 @@
-function decompose()
-X = [0 0; 0 20; 10 50; 20 20; 20 0];
+function centroids = decompose(X)
+% X = [0 0; 0 20; 10 50; 20 20; 20 0];
 
 pg = polyshape(X(:,1),X(:,2));
 tr = triangulation(pg);
 
-plot(pg);
-title('Input shape');
+% plot(pg);
+% title('Input shape');
 
 pdem = createpde;
 
@@ -13,23 +13,27 @@ pdem.geometryFromMesh(tr.Points', tr.ConnectivityList')
 mesh = generateMesh(pdem, 'GeometricOrder', 'linear', 'Hmax', 2, 'Hgrad', 1.01);
 
 % TODO: Preallocate array size
-points = [];
+centroids = [];
 
 for elem = mesh.Elements()
-    % Convert to using the number of rows 
-    pos_one = [mesh.Nodes(:,elem(1,:))'; mesh.Nodes(:,elem(2,:))'; mesh.Nodes(:,elem(3,:))'];
-
-    x_cent = sum(pos_one(:,1)) / 3;
-    y_cent = sum(pos_one(:,2)) / 3;
+    edges = [mesh.Nodes(:,elem(1,:))'; mesh.Nodes(:,elem(2,:))'; mesh.Nodes(:,elem(3,:))'];
     
-    points = [points; [x_cent, y_cent]];
+    % Calculate centroids
+    x_cent = sum(edges(:,1)) / 3;
+    y_cent = sum(edges(:,2)) / 3;
+    
+    centroids = [centroids; [x_cent, y_cent]];
 end
+
+return;
+
+% TODO: Remove from production
 
 % Plot overlayed pde and scatter
 figure
 hold on
 pdeplot(pdem, 'ElementLabels','on');
-scatter(points(:,1), points(:,2));
+scatter(centroids(:,1), centroids(:,2));
 title('Centre points of each element');
 hold off
 
@@ -40,5 +44,5 @@ title('Generated mesh');
 
 % Centre points
 figure
-scatter(points(:,1), points(:,2));
+scatter(centroids(:,1), centroids(:,2));
 title('Centre points of elements');
