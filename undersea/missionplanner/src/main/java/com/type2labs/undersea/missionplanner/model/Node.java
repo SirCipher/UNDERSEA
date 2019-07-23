886592ab61506b395ecc6f029b4fa603b84d5fe4
SirCipher <tklapwijk92@gmail.com>
2019-07-24 00:11:35 +0100

    Removes model project

--- undersea/missionplanner/src/main/java/com/type2labs/undersea/missionplanner/model/Node.java
+++ undersea/missionplanner/src/main/java/com/type2labs/undersea/missionplanner/model/Node.java
@@ -2,6 +2,8 @@ package com.type2labs.undersea.missionplanner.model;
 
 //import com.type2labs.undersea.Agent;
 
+import com.type2labs.undersea.Agent;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +14,7 @@ public class Node {
 
     private Vector vector;
     private NodeStatus nodeStatus;
-//    private List<Agent> agentsAssigned = new ArrayList<>();
+    private List<Agent> agentsAssigned = new ArrayList<>();
 
     public enum NodeStatus {
         UNVISITED, VISITED, POINT_OF_INTEREST
