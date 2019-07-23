89444e144e83e08a7a6ec61727963a1f0b721423
SirCipher <tklapwijk92@gmail.com>
2019-07-23 10:57:45 +0100

    Moves agent's projects to module agent

---
+++ undersea/agent/src/main/java/com/type2labs/undersea/App.java
@@ -0,0 +1,13 @@
+package com.type2labs.undersea;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
