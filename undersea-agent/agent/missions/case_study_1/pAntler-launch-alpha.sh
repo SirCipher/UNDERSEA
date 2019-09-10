aac3f788ab9ab8d12444512d29f1e4494bf2d52c
SirCipher <tk734@york.ac.uk>
2019-09-10 16:06:53 +0100

    Reformat

--- undersea-agent/agent/missions/case_study_1/pAntler-launch-alpha.sh
+++ undersea-agent/agent/missions/case_study_1/pAntler-launch-alpha.sh
@@ -1,1 +1,1 @@
-pAntler meta_vehicle_alpha.moos> /dev/null 2>&1
+pAntler meta_vehicle_alpha.moos >/dev/null 2>&1
--- undersea-agent/agent/missions/case_study_1/pAntler-launch-foxtrot.sh
+++ undersea-agent/agent/missions/case_study_1/pAntler-launch-foxtrot.sh
@@ -1,1 +1,1 @@
-pAntler meta_vehicle_foxtrot.moos> /dev/null 2>&1
+pAntler meta_vehicle_foxtrot.moos >/dev/null 2>&1
